a26e1b923fe4231f97dfd413d9570aa4a03b0e7a
Melisa Vacca <melisa.vacca@gmail.com>
2021-08-12 16:50:03 -0400

    updating query for deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -48,3 +48,27 @@ ORDER BY COUNT(title) DESC;
 DROP TABLE retiring_titles;
 
 SELECT * FROM retiring_titles;
+
+
+SELECT DISTINCT ON (emp_no)employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship
+FROM employees
+INNER JOIN dept_emp
+ON (employees.emp_no = dept_emp.emp_no)
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (dept_emp.to_date = '9999-01-01')
+AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no;
+
+SELECT * FROM mentorship;
+
+DROP TABLE mentorship
+
+
